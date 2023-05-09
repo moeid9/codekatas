82bcb936587f0cc3d6965ae60681a19b71ca6835
Moeid Haider <moeidhaider9@gmail.com>
2023-05-09 15:15:14 +0100

    added basic tests

--- odd_or_even/odd_or_even.test.js
+++ odd_or_even/odd_or_even.test.js
@@ -1,1 +1,10 @@
+const check = require("./odd_or_even");
 
+describe("Check odd_or_even", () => {
+  it("should return 'even' if number is even", () => {
+    expect(check(2)).toBe("even");
+  });
+  it("should return 'odd' if number is odd", () => {
+    expect(check(3)).toBe("odd");
+  });
+});
