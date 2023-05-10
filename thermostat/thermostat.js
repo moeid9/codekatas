ddcaf3b186fd4a8f61c8de4a8921864f15f0a3d6
Moeid Haider <moeidhaider9@gmail.com>
2023-05-10 22:51:26 +0100

    implemented the more tests and methods

--- thermostat/thermostat.js
+++ thermostat/thermostat.js
@@ -11,6 +11,33 @@ class Thermostat {
       this.currentTemp += 1;
     }
   }
+  down() {
+    if (this.currentTemp > 10) {
+      this.currentTemp -= 1;
+    }
+  }
+
+  getTemperature() {
+    return this.currentTemp;
+  }
+
+  setPowerSavingMode(OnOff) {
+    return (this.powerSave = OnOff);
+  }
+
+  reset() {
+    return (this.currentTemp = 20);
+  }
+
+  energyUsage() {
+    if (this.currentTemp < 18) {
+      return "low usage";
+    } else if (this.currentTemp <= 25) {
+      return "medium usage";
+    } else {
+      return "high usage";
+    }
+  }
 }
 
 module.exports = Thermostat;
